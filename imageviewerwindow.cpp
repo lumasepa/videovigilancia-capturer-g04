f1339bf1a1209e882ffd7fe89bd51cd61dda661a
Sergio Medina Toledo <lumasepa@gmail.com>
2013-02-28 12:18:47 +0000

    Hilo de trabajo funcionando

--- imageviewerwindow.cpp
+++ imageviewerwindow.cpp
@@ -6,6 +6,8 @@
 #include "qimagereader.h"
 #include "qpixmap.h"
 #include <QMovie>
+#include <QDebug>
+#include <QImage>
 
 #include <QWaitCondition>
 #include <QMutex>
@@ -17,10 +19,27 @@ ImageViewerWindow::ImageViewerWindow(QWidget *parent) :
     ui->setupUi(this);
 
     QObject::connect(&video,SIGNAL(updated(const QRect&)),this,SLOT(on_movie_updated(const QRect&)));
+    // Pasar la petición de procesar el frame
+    QObject::connect(this, SIGNAL(img_ProcesorRequest(const QImage &)),
+        &imageProcesor_, SLOT(process_image(const QImage &)));
+
+    // Ser notificado cuando el frame ha sido procesado
+    QObject::connect(&imageProcesor_, SIGNAL(send_image(const QImage &)),
+       this, SLOT(img_Procesed(const QImage &)));
+
+    // Migrar la instancia de imageProcesor al hilo de trabajo
+    imageProcesor_.moveToThread(&workingThread_);
+
+    // Iniciar el hilo de trabajo
+    workingThread_.start();
 }
 
 ImageViewerWindow::~ImageViewerWindow()
 {
+    // Le decimos al bucle de mensajes del hilo que se detenga
+    workingThread_.quit();
+    // Ahora esperamos a que el hilo de trabajo termine
+    workingThread_.wait();
     delete ui;
 }
 
@@ -40,32 +59,42 @@ void ImageViewerWindow::on_action_Abrir_triggered()
 
     if (!fileName.isEmpty())
     {
+        // Para el video
         video.stop();
+
+        // Pone como nuevo video fileName
         video.setFileName(fileName);
+
+        // Inicia el video
         video.start();
-
-        QWaitCondition sleep;
-        QMutex mutex;
-        sleep.wait(&mutex,20000);
-
-        /*QFile file(fileName);
-
-        if (!file.open(QIODevice::ReadOnly))
-        {
-            QMessageBox::critical(this, tr("Error"),tr("No se pudo abrir el archivo"));
-            return;
-        }
-
-        QImageReader imgc(&file);
-
-        QPixmap img = QPixmap::fromImageReader(&imgc);
-
-        ui->Imagen->setPixmap(img);
-        file.close();*/
+        qDebug() << "Inicio de video";
     }
 }
 
 void ImageViewerWindow::on_movie_updated(const QRect&)
 {
-    ui->Imagen->setPixmap(video.currentPixmap());
+    //ui->Imagen->setPixmap(video.currentPixmap());
+    //le manda al hilo la imagen para procesarla
+    QImage img = video.currentImage();
+    qDebug() << "emitiendo señal de intento de proceso";
+    emit img_ProcesorRequest(img);
 }
+
+void ImageViewerWindow::img_Procesed(const QImage &image)
+{
+    //recive la imagen del hilo ya procesada y la muestra
+    qDebug() << "Mostrando imagen";
+    QPixmap img;
+    img.convertFromImage(image);
+    ui->Imagen->setPixmap(img);
+}
+
+void Image_Thread::process_image(const QImage &image)
+{
+    //procesa la imagen
+    qDebug() << "Procesando la imagen";
+
+    //manda la imagen ya procesada al hilo principal
+    emit send_image(image);
+}
+
--- imageviewerwindow.h
+++ imageviewerwindow.h
@@ -3,10 +3,24 @@
 
 #include <QMainWindow>
 #include <QMovie>
+#include <QThread>
 
 namespace Ui {
 class ImageViewerWindow;
 }
+
+class Image_Thread : public QObject
+{
+    Q_OBJECT
+
+    signals:
+        // Señal emitida cuando el frame ha sido procesado
+        void send_image(const QImage &image);
+
+    public slots:
+        // Método encargado del ordenamiento
+        void process_image(const QImage &image);
+};
 
 class ImageViewerWindow : public QMainWindow
 {
@@ -15,7 +29,9 @@ class ImageViewerWindow : public QMainWindow
 public:
     explicit ImageViewerWindow(QWidget *parent = 0);
     ~ImageViewerWindow();
-    
+signals:
+    void img_ProcesorRequest(const QImage &image);
+
 private slots:
     void on_BtSalir_clicked();
 
@@ -25,9 +41,14 @@ private slots:
 
     void on_movie_updated(const QRect&);
 
+    void img_Procesed(const QImage &image);
 private:
+    QThread workingThread_;
+    Image_Thread imageProcesor_;
     Ui::ImageViewerWindow *ui;
     QMovie video;
 };
+
+
 
 #endif // IMAGEVIEWERWINDOW_H
